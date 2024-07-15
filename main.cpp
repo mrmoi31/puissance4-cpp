bfa68e0de0fda5960ffa2ca21b5b060a76cfcd32
mrmoi31 <dodo.leva.dl@gmail.com>
2024-07-15 10:55:59 +0200

    start victoire

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main() {
     int p = 0;
     char colonne = 'Z';
     int col = 0;
-    std::string grid[6][7] = {
+    char grid[6][7] = {
         //"A" "B" "C" "D" "E" "F" "G" 
-        {" ", " ", " ", " ", " ", " ", " "},
-        {" ", " ", " ", " ", " ", " ", " "},
-        {" ", " ", " ", " ", " ", " ", " "},
-        {" ", " ", " ", " ", " ", " ", " "},
-        {" ", " ", " ", " ", " ", " ", " "},
-        {" ", " ", " ", " ", " ", " ", " "},        
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},        
     };
 
     // boucle principale
@@ -56,7 +56,7 @@ int main() {
 
         // gestion gravité
         for (int i = 5; i >= 0; i--) {
-            if (grid[i][col] == " ") {
+            if (grid[i][col] == ' ') {
                 // saisie en fonction du joueur
                 if (players[p%2] == 1) {
                     grid[i][col] = place[0];
@@ -72,10 +72,53 @@ int main() {
 
         // gestion victoire
 
+        // gestion victoire lignes
+        int count = 0;
+        for (int i = 0; i <= 6; ++i){
+            for (int j = 0; j <= 6; ++j){
+                if (grid[i][j] == grid[i][j+1] && grid[i][j] == place[p%2]){
+                    count ++;
+                    if (count == 4){
+                        game_won = true;
+                        std::cout << "hassoul";
+                        std::cout << "\n\nPlayer " << players[p%2] << " gagne la partie\n";
+                        break;
+                    }
+                }
+            }
+        }
 
-        p++;
+        // gestion victoire colonnes
+        count = 0;
+        for (int i = 0; i <= 6; ++i){
+            for (int j = 0; j <= 5; ++j){
+                if (grid[j][i] == grid[j+1][i] && grid[i][j] == place[p%2]){
+                    if (count == 3){
+                        game_won = true;
+                        std::cout << "macaque";
+                        std::cout << "\n\nPlayer " << players[p%2] << " gagne la partie\n";
+                        break;
+                    }
+                }
+            }
+        }
 
-        std::cout << "game_won : " << game_won <<"\n\n";
+        //p++;
+
+        std::cout << "\n\ngame_won : " << game_won <<"\n\n";
     }
+
+        // fonction de display
+        std::cout << "    A   B   C   D   E   F   G \n";
+        for (int j = 0; j < 6; ++j){
+            std::cout << j+1 << " | ";
+            for (int i = 0; i < 7; ++i){
+                std::cout << grid[j][i] << " | ";
+            }
+            std::cout << "\n";
+            std::cout << "  -----------------------------\n";
+        }
+        // end fonction
+
     return 0;
 }
